dd54f0c5f6015656dd4ed4b2423484407d4e59b9
meddil <med.maaoui.007@gmail.com>
2024-03-16 01:31:57 -0400

    commit 80

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Layout from './Components/Landing_Page/Landing_Page';
 import Navbar from './Components/Navbar/Navbar';
 import SignUp from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
+import InstantConsultation from './Components/Instant Consultation/InstantConsultation'
+
+
 function App() {
   return (
     <div className="App">
@@ -15,6 +18,8 @@ function App() {
           <Route path="/" element={<Layout />} />
           <Route path="/Sign_Up" element={<SignUp />} />
           <Route path="/Login" Component={Login} />
+          <Route path="/InstantConsultation" Component={InstantConsultation} />
+
 
         </Routes>
       </BrowserRouter>
