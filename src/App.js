5315a8c94d371e5d5ee1917f9a39e1a6f9ea94c2
meddil <med.maaoui.007@gmail.com>
2024-03-17 20:08:42 -0400

    commit 870

--- src/App.js
+++ src/App.js
@@ -11,18 +11,19 @@ import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch'
 import DoctorCard from './Components/DoctorCard/DoctorCard'
 import AppointmentForm from './Components/AppointmentForm/AppointmentForm'
 import Notification from './Components/Notification/Notification';
+import ReviewForm from './Components/ReviewForm/ReviewForm';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-
         <Navbar />
         <Notification>
         <Routes>
           <Route path="/" element={<Layout />} />
           <Route path="/Sign_Up" element={<SignUp />} />
           <Route path="/Login" Component={Login} />
+          <Route path="/reviews" element={<ReviewForm />} />
           <Route path="/instant-consultation" element={<InstantConsultation />} /> 
           <Route path="/FindDoctorSearch" Component={FindDoctorSearch} /> 
           <Route path="/DoctorCard" Component={DoctorCard} />         
